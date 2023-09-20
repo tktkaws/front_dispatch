a10249b4700f669d2155b6ec664e57b987e2dbf2
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-20 14:52:16 +0900

    Fix: 背景を変更

--- components/layout.js
+++ components/layout.js
@@ -3,40 +3,40 @@ import Footer from "components/footer";
 import styles from "styles/layout.module.css";
 
 export default function Layout({ children }) {
-  console.log(children.props);
-  let backgroundColor;
+  // console.log(children.props);
+  // let backgroundColor;
 
-  if (children.props.name === "HTML") {
-    backgroundColor = "#C1E0D0";
-  } else if (children.props.name === "CSS") {
-    backgroundColor = "#FCDFE3";
-  } else if (children.props.name === "JavaScript") {
-    backgroundColor = "#F6EEBE";
-  } else {
-    backgroundColor = "#D5E9ED";
-  }
-  if (children.props.categories) {
-    if (children.props.categories[0].name === "HTML") {
-      backgroundColor = "#C1E0D0";
-    } else if (children.props.categories[0].name === "CSS") {
-      backgroundColor = "#FCDFE3";
-    } else if (children.props.categories[0].name === "JavaScript") {
-      backgroundColor = "#F6EEBE";
-    } else {
-      backgroundColor = "#D5E9ED";
-    }
-  }
-  const style = {
-    backgroundImage: "url('/bg_noise01.png')",
-    backgroundRepeat: "repeat",
-    backgroundSize: "64px 64px",
-    backgroundPosition: "left top",
-    backgroundColor: backgroundColor,
-    width: "100%",
-  };
+  // if (children.props.name === "HTML") {
+  //   backgroundColor = "#C1E0D0";
+  // } else if (children.props.name === "CSS") {
+  //   backgroundColor = "#FCDFE3";
+  // } else if (children.props.name === "JavaScript") {
+  //   backgroundColor = "#F6EEBE";
+  // } else {
+  //   backgroundColor = "#D5E9ED";
+  // }
+  // if (children.props.categories) {
+  //   if (children.props.categories[0].name === "HTML") {
+  //     backgroundColor = "#C1E0D0";
+  //   } else if (children.props.categories[0].name === "CSS") {
+  //     backgroundColor = "#FCDFE3";
+  //   } else if (children.props.categories[0].name === "JavaScript") {
+  //     backgroundColor = "#F6EEBE";
+  //   } else {
+  //     backgroundColor = "#D5E9ED";
+  //   }
+  // }
+  // const style = {
+  //   backgroundImage: "url('/bg_noise01.png')",
+  //   backgroundRepeat: "repeat",
+  //   backgroundSize: "64px 64px",
+  //   backgroundPosition: "left top",
+  //   backgroundColor: backgroundColor,
+  //   width: "100%",
+  // };
   return (
     <>
-      <div style={style}>
+      <div>
         <div className={styles.container}>
           <Header />
           <main>{children}</main>
