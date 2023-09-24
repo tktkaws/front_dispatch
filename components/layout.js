8fd126cd6a48d28d337e49aa0c9f45a793f3aa38
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-24 11:22:32 +0900

    Fix:背景色を変更

--- components/layout.js
+++ components/layout.js
@@ -4,39 +4,55 @@ import styles from "styles/layout.module.css";
 
 export default function Layout({ children }) {
   // console.log(children.props);
-  // let backgroundColor;
+  let backgroundColor;
 
-  // if (children.props.name === "HTML") {
-  //   backgroundColor = "#C1E0D0";
-  // } else if (children.props.name === "CSS") {
-  //   backgroundColor = "#FCDFE3";
-  // } else if (children.props.name === "JavaScript") {
-  //   backgroundColor = "#F6EEBE";
-  // } else {
-  //   backgroundColor = "#D5E9ED";
-  // }
-  // if (children.props.categories) {
-  //   if (children.props.categories[0].name === "HTML") {
-  //     backgroundColor = "#C1E0D0";
-  //   } else if (children.props.categories[0].name === "CSS") {
-  //     backgroundColor = "#FCDFE3";
-  //   } else if (children.props.categories[0].name === "JavaScript") {
-  //     backgroundColor = "#F6EEBE";
-  //   } else {
-  //     backgroundColor = "#D5E9ED";
-  //   }
-  // }
-  // const style = {
-  //   backgroundImage: "url('/bg_noise01.png')",
-  //   backgroundRepeat: "repeat",
-  //   backgroundSize: "64px 64px",
-  //   backgroundPosition: "left top",
-  //   backgroundColor: backgroundColor,
-  //   width: "100%",
-  // };
+  if (children.props.name === "HTML") {
+    backgroundColor = "#C1E0D0";
+  } else if (children.props.name === "CSS") {
+    backgroundColor = "#FCDFE3";
+  } else if (children.props.name === "JavaScript") {
+    backgroundColor = "#F6EEBE";
+  } else if (children.props.name === "WebGL") {
+    backgroundColor = "#7DBFC2";
+  } else if (children.props.name === "Astro") {
+    backgroundColor = "#EAA75E";
+  } else if (children.props.name === "Accessibility") {
+    backgroundColor = "#FAD77B";
+  } else if (children.props.name === "Private") {
+    backgroundColor = "#D9DFBA";
+  } else {
+    backgroundColor = "#D5E9ED";
+  }
+  if (children.props.categories) {
+    if (children.props.categories[0].name === "HTML") {
+      backgroundColor = "#C1E0D0";
+    } else if (children.props.categories[0].name === "CSS") {
+      backgroundColor = "#FCDFE3";
+    } else if (children.props.categories[0].name === "JavaScript") {
+      backgroundColor = "#F6EEBE";
+    } else if (children.props.categories[0].name === "WebGL") {
+      backgroundColor = "#7DBFC2";
+    } else if (children.props.categories[0].name === "Astro") {
+      backgroundColor = "#EAA75E";
+    } else if (children.props.categories[0].name === "Accessibility") {
+      backgroundColor = "#FAD77B";
+    } else if (children.props.categories[0].name === "Private") {
+      backgroundColor = "#D9DFBA";
+    } else {
+      backgroundColor = "#D5E9ED";
+    }
+  }
+  const style = {
+    backgroundImage: "url('/bg_noise01.png')",
+    backgroundRepeat: "repeat",
+    backgroundSize: "64px 64px",
+    backgroundPosition: "left top",
+    backgroundColor: backgroundColor,
+    width: "100%",
+  };
   return (
     <>
-      <div>
+      <div style={style}>
         <div className={styles.container}>
           <Header />
           <main>{children}</main>
